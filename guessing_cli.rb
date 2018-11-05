12b55bd27f170436bc5ca1b694b8d9de25fbf8a0
Daniel Hamilton <danhamilton16@gmail.com>
2018-11-05 20:20:46 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,6 +6,7 @@ def run_guessing_game
   gameend=false
   number=rand(1..6)
   puts "Guess a number between 1 and 6."
+  puts "(Hint, it's #{number})"
   while gameend==false
     guess=gets.chomp
     if !guess.is_a?(Integer)
